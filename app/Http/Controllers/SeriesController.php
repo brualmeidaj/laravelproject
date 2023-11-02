1eeef6f3fffb25b04a93836dc545a4e63f0ac994
Bruna Almeida <202003317985@alunos.estacio.br>
2023-11-02 13:39:26 -0300

    controllers

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    //
+
+    //existem varias formas de usar o verbo get, nesse caso usaremos o metodo index, mas existe o create, store, etc...-https://laravel.com/docs/9.x/controllers#resource-controllers
+    public function index()
+    {
+        // array de séries
+        $series = [
+            'Punisher',
+            'Lost',
+            'Grey\'s Anatomy'
+        ];
+
+        $html = '<ul>';
+
+        foreach ($series as $serie) {
+            $html .= "<li>$serie</li>";
+        }
+
+        $html .= '</ul>';
+
+        echo $html;
+    }
 }
